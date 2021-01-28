22d1e46fb3f1258afbc1805c7453ecc6f9b275a1
pedja <predragcokulov@gmail.com>
2021-01-28 16:09:52 +0100

    added another method for LocationServices

--- locationServices/src/gms/java/com/computerrock/location/core/LocationServices.kt
+++ locationServices/src/gms/java/com/computerrock/location/core/LocationServices.kt
@@ -1,10 +1,16 @@
 package com.computerrock.location.core
 
 import android.app.Activity
-import com.google.android.gms.location.LocationServices
+import android.content.Context
 
 object LocationServices {
+    @JvmStatic
     fun getFusedLocationProviderClient(activity: Activity): FusedLocationProviderClient {
-        return FusedLocationProviderClient(LocationServices.getFusedLocationProviderClient(activity))
+        return FusedLocationProviderClient(com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(activity))
+    }
+
+    @JvmStatic
+    fun getFusedLocationProviderClient(context: Context): FusedLocationProviderClient {
+        return FusedLocationProviderClient(com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(context))
     }
 }
--- locationServices/src/hms/java/com/computerrock/location/core/LocationServices.kt
+++ locationServices/src/hms/java/com/computerrock/location/core/LocationServices.kt
@@ -1,10 +1,16 @@
 package com.computerrock.location.core
 
 import android.app.Activity
-import com.huawei.hms.location.LocationServices
+import android.content.Context
 
 object LocationServices {
+    @JvmStatic
     fun getFusedLocationProviderClient(activity: Activity): FusedLocationProviderClient {
-        return FusedLocationProviderClient(LocationServices.getFusedLocationProviderClient(activity))
+        return FusedLocationProviderClient(com.huawei.hms.location.LocationServices.getFusedLocationProviderClient(activity))
+    }
+
+    @JvmStatic
+    fun getFusedLocationProviderClient(context: Context): FusedLocationProviderClient {
+        return FusedLocationProviderClient(com.huawei.hms.location.LocationServices.getFusedLocationProviderClient(context))
     }
 }
