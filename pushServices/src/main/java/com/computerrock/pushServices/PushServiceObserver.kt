fb20a94a3b70b0e498a86f9354affb7248f68f6a
pedja <predragcokulov@gmail.com>
2021-01-29 11:20:14 +0100

    removed unnecessary method from PushServiceObserver

--- pushServices/src/main/java/com/computerrock/pushServices/PushServiceObserver.kt
+++ pushServices/src/main/java/com/computerrock/pushServices/PushServiceObserver.kt
@@ -4,5 +4,4 @@ package com.computerrock.pushServices
 interface PushServiceObserver {
     fun onNewToken(token:String)
     fun onMessageReceived(message: RemoteMassage)
-    fun onRemoteMessageReceived(message: RemoteMassage)
 }
